1d0cc50ebecf66c7b6a8c4b26e3640c9450a636e
badalkainth <badalkainth06@gmail.com>
2023-06-24 22:16:12 +0530

    mini_project_3

---
+++ app.mjs
@@ -0,0 +1,61 @@
+import { food } from "./food.mjs";
+
+function listAll() {
+  return food;
+}
+
+function listWithCategories(category) {
+  return food.filter((x) => x.category === category);
+}
+
+function listAllVegetables() {
+  return listWithCategories("Vegetable");
+}
+
+function listAllFruits() {
+  return listWithCategories("Fruit");
+}
+
+function listAllProteins() {
+  return listWithCategories("Protein");
+}
+function listAllNuts() {
+  return listWithCategories("Nuts");
+}
+
+function listAllGrains() {
+  return listWithCategories("Grain");
+}
+
+function listAllDairy() {
+  return listWithCategories("Dairy");
+}
+
+function above100Calories() {
+  return food.filter((food) => food.calorie > 100);
+}
+
+function below100Calories() {
+  return food.filter((food) => food.calorie < 100);
+}
+
+function listHighToLow() {
+  return food.sort((a, b) => b.protiens - a.protiens);
+}
+
+function listLowToHigh() {
+  return food.sort((a, b) => a.cab - b.cab);
+}
+
+// / /  Solutions / /
+// console.log(listAll());
+// console.log(listAllVegetables());
+// console.log(listAllFruits());
+// console.log(listAllProteins());
+// console.log(listAllNuts());
+// console.log(listAllGrains());
+// console.log(listAllDairy());
+// console.log(above100Calories());
+// console.log(below100Calories());
+// console.log(listHighToLow());
+// console.log(listLowToHigh());
